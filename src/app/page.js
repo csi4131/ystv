79ac36be7a14a2c51bd241d50815acaaa0db830d
DK <87054926+kdkcode@users.noreply.github.com>
2023-06-04 10:36:32 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,25 @@
-'use client'
-import React from 'react'
-import { HlsPlayer } from '@/components/HlsPlayer'
+'use client';
+import React, { useState } from 'react';
+import { HlsPlayer } from '@/components/HlsPlayer';
+import { HlsContext } from '@/store/HlsContext';
+import Menu from '@/components/Menu';
 
 export default function Home() {
+  const handleToggleVideo = (videoVisible) => {
+    // 영상 숨기기/보이기 기능을 처리하는 로직을 추가하세요
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <HlsPlayer
-        src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-        controls={true}
-        width="100%"
-        height="100%"
-      />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <HlsContext.Provider value={{ metric: { events: [] } }}>
+        <HlsPlayer
+          src="https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8"
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+        <Menu onToggleVideo={handleToggleVideo} />
+      </HlsContext.Provider>
     </main>
-  )
+  );
 }
